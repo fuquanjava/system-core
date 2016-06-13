194ca12c66724bfee83f45f3fa95b05cfcb33da4
付泉 <3040408640@qq.com>
2016-06-13 10:11:25 +0800

    1. sort

--- data-struct/src/main/java/data/struct/sort/QuickSort.java
+++ data-struct/src/main/java/data/struct/sort/QuickSort.java
@@ -1,47 +1,47 @@
 package data.struct.sort;
 
+import java.util.Arrays;
+
 /**
  * fuquanemail@gmail.com
  * 2016/6/11
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] array = {2, 3, 1};
-        //quickSort(array, 0, array.length - 1);
-
-        swap(array,0, array.length -1);
-
-        System.err.println(array);
-
+        int[] array = {2, 3, 4, 5, 1};
+        quickSort(array, 0, array.length - 1);
+        System.err.println(Arrays.toString(array));
     }
 
     static void quickSort(int[] array, int lo, int hi) {
-        System.out.println("hi-lo = " + (hi - lo));
+
         if (lo >= hi) {
             return;
         }
-
         int mi = createPivot(array, lo, hi);
-        quickSort(array, lo, mi);
+        quickSort(array, lo, mi-1);
         quickSort(array, mi + 1, hi);
     }
 
     private static int createPivot(int[] array, int lo, int hi) {
-        int mi = array[0];
+        int mi = array[lo];
         while (lo < hi) {
-            while ((lo < hi) && mi <= array[hi]) {
+            while ((lo < hi) && array[hi] >= mi) {
                 hi--;
-                swap(array, lo, hi);
+            }
+            if (lo < hi) {
+                array[lo++] = array[hi];
+            }
+
+            while (lo < hi && array[lo] <= mi) {
+                lo++;
+            }
+            if (lo < hi) {
+                array[hi--] = array[lo];
             }
         }
+        array[lo] = mi;
         return lo;
-
-    }
-
-    private static void swap(int[] array, int lo, int hi) {
-        int tmp = array[lo];
-        array[lo] = array[hi];
-        array[hi] = tmp;
 
     }
 }
