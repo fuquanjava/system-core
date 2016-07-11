3a95269d7647461ccf664b4afcdc3f465998a068
fuquanemail@gmail.com <fuquanemail@gmail.com>
2016-07-11 23:28:19 +0800

    1 thread

---
+++ thread/src/main/java/net/jcip/examples/DoubleCheckedLocking.java
@@ -0,0 +1,28 @@
+package net.jcip.examples;
+
+
+
+/**
+ * DoubleCheckedLocking
+ * <p/>
+ * Double-checked-locking antipattern
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class DoubleCheckedLocking {
+    private static Resource resource;
+
+    public static Resource getInstance() {
+        if (resource == null) {
+            synchronized (DoubleCheckedLocking.class) {
+                if (resource == null)
+                    resource = new Resource();
+            }
+        }
+        return resource;
+    }
+
+    static class Resource {
+
+    }
+}
